a568cc6e3a9ed8352f32a09b63ab7706199ddece
Theo Molteno <theo@molteno.net>
2021-07-11 21:48:47 +1200

    initial commit

---
+++ Makefile
@@ -0,0 +1,26 @@
+BUNDLENAME=tilteq.lv2
+INSTALLDIR=~/.lv2/
+
+.PHONY: install bundle
+
+tilteq.so:
+	g++ -o tilteq.so  -shared -fPIC -DPIC tilteq.cpp `pkg-config --cflags lv2-plugin`
+
+clean:
+	rm tilteq.so
+
+bundle: tilteq.so
+	mkdir -p 	$(BUNDLENAME)
+	mv tilteq.so  $(BUNDLENAME)
+	cp manifest.ttl tilteq.ttl $(BUNDLENAME)
+
+install: bundle
+	mv $(BUNDLENAME) $(INSTALLDIR)
+
+uninstall:
+	rm -rf $(INSTALLDIR)/$(BUNDLENAME)
+
+reinstall: uninstall install
+
+test:
+	jalv https://github.com/moltenot/lv2-tilteq
---
+++ tilteq.cpp
@@ -0,0 +1,23 @@
+#include <lv2plugin.hpp>
+
+// have to make sure these are the same as in tilteq.ttl
+#define TILT_PORT_INDEX 0
+#define INPUT_PORT_INDEX 1
+#define OUTPUT_PORT_INDEX 2
+
+using namespace LV2;
+
+class TiltEQ : public Plugin<TiltEQ>
+{
+public:
+    TiltEQ(double rate) 
+        : Plugin<TiltEQ>(3) {} // since we have 3 ports
+
+    void run(uint32_t sample_count) { // currently pass through
+        for (uint32_t i=0; i<sample_count; ++i) {
+            p(OUTPUT_PORT_INDEX)[i] = p(INPUT_PORT_INDEX)[i];
+        }
+    }
+};
+
+unsigned _ = TiltEQ::register_class("https://github.com/moltenot/lv2-tilteq");
