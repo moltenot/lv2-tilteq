f69f6190802603c53da01e020a67e0a8023b980c
Theo Molteno <theo@molteno.net>
2021-07-16 17:46:13 +1200

    added prints

--- tilteq.cpp
+++ tilteq.cpp
@@ -11,13 +11,32 @@ class TiltEQ : public Plugin<TiltEQ>
 {
 public:
     TiltEQ(double rate)
-        : Plugin<TiltEQ>(3) {} // since we have 3 ports
+        : Plugin<TiltEQ>(3)
+    {
+        printf("created tilteq with rate %f\n", rate);
+    } // since we have 3 ports
 
     void run(uint32_t sample_count)
     { // currently pass through
         for (uint32_t i = 0; i < sample_count; ++i)
         {
+            // printf("\ncalled run\n");
+            // print_nums(p(INPUT_PORT_INDEX), sample_count);
             p(OUTPUT_PORT_INDEX)[i] = p(INPUT_PORT_INDEX)[i];
+        }
+    }
+
+protected:
+    void print_nums(float *port, uint32_t n)
+    {
+        int num_cols = 6;
+        for (uint32_t i = 0; i < n; i++)
+        {
+            printf("%f\t", port[i]);
+            if (i % num_cols == 0)
+            {
+                printf("\n");
+            }
         }
     }
 };
